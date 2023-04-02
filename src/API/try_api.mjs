125af96cbfd99d86e4cea4492b59729f49d656d2
Benjamin Jonen <bjonen@gmail.com>
2023-04-02 03:21:16 +0200

    Expand try_api

--- src/API/try_api.mjs
+++ src/API/try_api.mjs
@@ -1,10 +1,36 @@
 // Run with node try_api.mjs
 
-import { _getUsers } from "./_DATA.js";
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "./_DATA.js";
 //const parse = require("./_DATA.mjs");
 
-async function getUsers() {
-  const response = await _getUsers();
-  console.log(response);
+async function getdata() {
+  var response = await _getUsers();
+  console.log("users", response);
+  response = await _getQuestions();
+  console.log("questions", response);
+  console.log(response["vthrdm985a262al8qx3do"]["optionOne"]["votes"]);
+  response = await _saveQuestion({
+    optionOneText: "Be a good person",
+    optionTwoText: "Be a bad person",
+    author: "tylermcginnis",
+  });
+
+  const newid = response.id;
+  console.log("newid", newid);
+  response = await _getQuestions();
+  console.log("questions", response);
+  response = await _saveQuestionAnswer({
+    authedUser: "tylermcginnis",
+    qid: newid,
+    answer: "optionOne",
+  });
+  response = await _getQuestions();
+  console.log("questions", response);
+  console.log(response[newid]["optionOne"]["votes"]);
 }
-getUsers();
+getdata();
